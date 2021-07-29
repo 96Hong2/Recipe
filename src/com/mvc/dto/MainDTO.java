83528025add96696c4e8854542f573dc821cba57
eukeun <g9016@naver.com>
2021-07-29 17:24:50 +0900

    product, cart 테이블 관련 DTO 추가

--- src/com/mvc/dto/MainDTO.java
+++ src/com/mvc/dto/MainDTO.java
@@ -1,5 +1,67 @@
 package com.mvc.dto;
 
 public class MainDTO {
-	
+	private String productid;
+	private String productname;
+	private int price;
+	private int stock;
+	private String productdetail;
+	private String isdel;
+	private String userid;
+	private int productnumber;
+	private int totalprice;
+	public String getProductid() {
+		return productid;
+	}
+	public void setProductid(String productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getProductdetail() {
+		return productdetail;
+	}
+	public void setProductdetail(String productdetail) {
+		this.productdetail = productdetail;
+	}
+	public String getIsdel() {
+		return isdel;
+	}
+	public void setIsdel(String isdel) {
+		this.isdel = isdel;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public int getProductnumber() {
+		return productnumber;
+	}
+	public void setProductnumber(int productnumber) {
+		this.productnumber = productnumber;
+	}
+	public int getTotalprice() {
+		return totalprice;
+	}
+	public void setTotalprice(int totalprice) {
+		this.totalprice = totalprice;
+	}
 }
